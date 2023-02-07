03d8d88822f0587c3175b9090d66b75ecb988074
Taryn Chiang <tarynchiang@email.com>
2023-02-07 16:28:16 +0800

    change structure

--- src/components/Home/HomeBackground.tsx
+++ src/components/Home/HomeBackground.tsx
@@ -1,9 +1,9 @@
 import { Component } from "react";
-import styles from "./Background.module.scss";
+import styles from "./Home.module.scss";
 import ellipse from "../../assets/images/ellipse.svg";
 import triangle from "../../assets/images/triangle.svg";
 
-export default class Background extends Component {
+export default class HomeBackground extends Component {
   render() {
     return (
       <div className={styles.bg}>
