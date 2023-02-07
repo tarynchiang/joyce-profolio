9280f4172887983690eacf9cb56f33ac38aa9206
Taryn Chiang <tarynchiang@email.com>
2023-02-07 16:26:54 +0800

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./App.scss";
-
 import { Component } from "react";
-import Background from "./components/Background/Background";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import Birka from "./components/Birka";
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
         <NavBar />
-        <Background />
-        <Home />
+        <Routes>
+          <Route path={"joyce-profolio/"} element={<Home />} />
+          <Route path={"/joyce-profolio/birka"} element={<Birka />} />
+        </Routes>
       </div>
     );
   }
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./Project.module.scss";
 
 type ProjectType = {
@@ -9,9 +10,11 @@ type ProjectType = {
 export default function Project(props: ProjectType) {
   return (
     <div className={styles["project-container"]}>
-      <div className={styles["logo-container"]}>
-        <img src={process.env.PUBLIC_URL + props.icon} alt="project-logo" />
-      </div>
+      <Link to="/joyce-profolio/birka">
+        <div className={styles["logo-container"]}>
+          <img src={process.env.PUBLIC_URL + props.icon} alt="project-logo" />
+        </div>
+      </Link>
       <div className={styles["text-container"]}>
         <div className={styles.name}>{props.name}</div>
         <div className={styles.type}>{props.type}</div>
